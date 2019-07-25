004362cb02843472db898fd4fe475a454654840f
TzuyuLiu <a2647803@yahoo.com.tw>
2019-07-25 19:02:12 +0800

    Initial Commit

---
+++ Top20Pictures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top20Pictures
+//
+//  Created by 劉子瑜 on 2019/7/25.
+//  Copyright © 2019 劉子瑜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
